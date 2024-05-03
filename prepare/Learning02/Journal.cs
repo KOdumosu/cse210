9dd573823fb106e05f8342cf4be193011c6a7c8f
Kehinde Odumosu <odu23006@byui.edu>
2024-05-03 07:51:12 +0100

    Making team journal

---
+++ prepare/Learning02/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public list<Entry> _entries;
+
+    public void AddEntry(Entry newEntry) 
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void Savetofile(string file)
+    {
+
+    }
+
+    public void LoadfromFile(string file)
+    {
+        
+    }
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,25 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2024;
-        job1._endYear = 2029;
-
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Souls Tech";
-        job2._startYear = 2024;
-        job2._endYear = 2030;
-
-        Resume myResume = new Resume();
-        myResume._name = "Odumosu Kenny";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+        
+       Console.WriteLine("Hello Learning02 World");
     }
 
 }
