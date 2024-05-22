c6e87dee19a43fad070c1ae9898f745a1e9a72a3
Kehinde Odumosu <odu23006@byui.edu>
2024-05-22 16:57:39 +0100

    My Scripture memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        Scripture _scripture = new Scripture(2, "Thessalonians", 1, 10, 12, "10. Who died for us, that, whether we wake or sleep, we should alive together with him.\n11. Wherefore acomfort yourselves together, and bedify one another, even as also ye do.\n12. And we beseech you, brethren, to know them which alabour among you, and are over you in the Lord, and admonish you;");
+        Console.Clear();
+        _scripture.RenderedDisplay();
+        Console.WriteLine("\n\nPlease press 'enter' key to continue or type 'quit' to finish:");
+        while (true)
+        {
+            string user_input = Console.ReadLine();
+            if (user_input.ToLower() == "quit")
+            {
+                break;
+            }
+            if (!_scripture.HideWords())
+            {
+                break;
+            }
+
+
+            Console.Clear();
+            _scripture.RenderedDisplay();
+            Console.WriteLine("\n\nPlease press 'enter' key to continue or type 'quit' to finish:");
+        }
     }
+
 }
+
+// The program exceeds the core requirements by displaying a scripture with first epistle and second epistle and gave it a member variable position;
